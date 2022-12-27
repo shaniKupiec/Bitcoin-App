fa1217b2136c739d671b266b337000f88c99887f
shani kupiec <shani.kupiec@gmail.com>
2022-12-27 14:25:39 +0200

    signup page is done

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -10,5 +10,14 @@ export const SignUpPage = (props) => {
     props.history.push("/");
   };
 
-  return <button onClick={onExplore}>submit</button>;
+  return (
+    <section className="sign-up-page">
+      <section className="illustrations"></section>
+      <div className="title">Welcome to <span>Crypto</span>bits</div>
+      <div onClick={onExplore} className="explore pointer">
+        <span>Explore</span>
+        <div className="explore__icon"></div>
+      </div>
+    </section>
+  );
 };
